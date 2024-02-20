b444ae0ded2e7105300d68aa041d036f6d318c10
Garret Burnight <55258111+BlooperGMB@users.noreply.github.com>
2024-02-20 00:21:13 -0600

    Add files via upload
    I integrated all places on board code and trajectories with all 12 variations. The slow mode has been implemented on the first moves dealing with a purple pixel. I also cleared unused code segments and methods. I also did more things I don't remember.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/RedBackboard.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/RedBackboard.java
@@ -18,10 +18,6 @@ import java.util.List;
 // imported from followerPID
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
@@ -42,7 +38,6 @@ public class RedBackboard extends LinearOpMode {
     VisionPortal.Builder myVisionPortalBuilder;
     boolean USE_WEBCAM;
     TfodProcessor myTfodProcessor;
-    double MainSpeed;
     float tgeLocation;
     VisionPortal myVisionPortal;
     @Override
@@ -71,7 +66,6 @@ public class RedBackboard extends LinearOpMode {
         initTfod();
         // Set the minimum confidence at which to keep recognitions.
         myTfodProcessor.setMinResultConfidence((float) 0.6);
-        MainSpeed = 0.2;
         fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -80,6 +74,8 @@ public class RedBackboard extends LinearOpMode {
         arm2.setPosition(0.09);
          OperateClaw(0, 0);
          OperateClaw(1, 0);
+        double Distance_From_Board;
+        double Slide_Hieght;
         slide.setTargetPosition(0);
         slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         slide.setDirection(DcMotor.Direction.FORWARD);
@@ -107,30 +103,8 @@ public class RedBackboard extends LinearOpMode {
             telemetry.addData("location", tgeLocation);
         }
         telemetry.update();
-        tgeLocation = 1;
-        if (tgeLocation == 1) {
-                /* commenting out old code
-                // forward 1
-                MoveRobot(66.04, MainSpeed);
-                StrafeRobot(-9, MainSpeed);
-                TurnRobot(-46, MainSpeed);
-                MoveRobot(5, MainSpeed);
-                MoveRobot(-5, MainSpeed);
-                StrafeRobot(20, MainSpeed);
-                TurnRobot(46, MainSpeed);
-                StrafeRobot(10, MainSpeed);
-                // 180
-                TurnRobot(92, MainSpeed);
-                // strafe left to align
-                StrafeRobot(-9, MainSpeed);
-                MoveRobot(7, MainSpeed);
-                Place_On_Board(29, 4.7);
-                // strafe righ 1
-                StrafeRobot(75, MainSpeed);
-                // forward 1
-                MoveRobot(80, MainSpeed);
-                // time to brake
-                sleep(1000);           */
+        if (tgeLocation == 1) { //Location 1, Left Side
+
            TrajectorySequence trajSeq = drive.trajectorySequenceBuilder(startPose)
                     .forward(25,
                             SampleMecanumDrive.getVelocityConstraint(30, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
@@ -144,15 +118,14 @@ public class RedBackboard extends LinearOpMode {
                     .turn(Math.toRadians(-135))
                     .splineTo(new Vector2d(30,-28),0)
                     .build();
-            double Place_on_Board_Time = 0;
-            double Distance_From_Board = 4; // do not set me to 0 - I will kill your code
-            double Slide_Hieght = 4.5;
+            Distance_From_Board = 4; // do not set me to 0 - I will kill your code
+            Slide_Hieght = 4.5;
             TrajectorySequence On_Board = drive.trajectorySequenceBuilder (trajSeq.end())
-                        .addTemporalMarker(Place_on_Board_Time, () -> {
+                        .addTemporalMarker(0, () -> {
                             slide.setPower(0.5);
                             slide.setTargetPosition((int) (Slide_Hieght * 385));
                         })
-                        .addTemporalMarker(Place_on_Board_Time + 2, () -> {
+                        .addTemporalMarker(2, () -> {
                             arm1.setPosition(0.4);
                             arm2.setPosition(0.56);
                             dump.setPosition(0.49);
@@ -169,14 +142,14 @@ public class RedBackboard extends LinearOpMode {
                         })
                         .waitSeconds(1.5)
                         .back(Distance_From_Board)
-                        .addTemporalMarker(Place_on_Board_Time + 7.5, () -> {
+                        .addTemporalMarker(7.5, () -> {
                             OperateClaw(0, 0);
                             OperateClaw(1, 0);
                             arm1.setPosition(0.87);
                             arm2.setPosition(0.09);
                             dump.setPosition(0.3);
                         })
-                        .addTemporalMarker(Place_on_Board_Time + 9.5, () -> {
+                        .addTemporalMarker(9.5, () -> {
                             slide.setPower(-0.5);
                             slide.setTargetPosition((int) (0));
                         })
@@ -185,74 +158,118 @@ public class RedBackboard extends LinearOpMode {
                         .lineTo(new Vector2d(56,-56))
                         .build();
 
-            drive.followTrajectorySequence(trajSeq);
-           drive.followTrajectorySequence(On_Board);
-        }else if (tgeLocation == 2) {
-                /*
-                // forward 1
-                MoveRobot(66.04, MainSpeed);
-                MoveRobot(11, MainSpeed);
-                MoveRobot(-11, MainSpeed);
-                // turn left 90
-                TurnRobot(92, MainSpeed);
-                // Strafe to 2nd position
-                StrafeRobot(-4, MainSpeed);
-                MoveRobot(30, MainSpeed);
-                Place_On_Board(24, 4.5);
-                sleep(700);
-                // strafe righ 1
-                StrafeRobot(60, MainSpeed);
-                // forward 1
-                MoveRobot(100, MainSpeed);
-                // time to brake
-                sleep(1000); */
+                 drive.followTrajectorySequence(trajSeq);
+                 drive.followTrajectorySequence(On_Board);
+        }else if (tgeLocation == 2) { //Location 2, Middle
+
             TrajectorySequence trajSeq = drive.trajectorySequenceBuilder(startPose)
-                    .forward(28)
+                    .forward(28,
+                            SampleMecanumDrive.getVelocityConstraint(30, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                            SampleMecanumDrive.getAccelerationConstraint(30)
+                            //Limits to 30 in/s and 30 in/s^2
+                    )
                     .back(2)
                     .turn(Math.toRadians(-90))
-                    .splineTo(new Vector2d(34,-34),0)
-                    .addDisplacementMarker(()-> {
-                        //Put the "Place on board "code here.
-                        // Place_On_Board(24, 4.5)
+                    .lineTo(new Vector2d(34,-34))
+                    .build();
+            Distance_From_Board = 4; // do not set me to 0 - I will kill your code
+            Slide_Hieght = 4.5;
+            TrajectorySequence On_Board = drive.trajectorySequenceBuilder (trajSeq.end())
+                    .addTemporalMarker(0, () -> {
+                        slide.setPower(0.5);
+                        slide.setTargetPosition((int) (Slide_Hieght * 385));
                     })
-                    .splineTo(new Vector2d (60,-60),0)
+                    .addTemporalMarker(2, () -> {
+                        arm1.setPosition(0.4);
+                        arm2.setPosition(0.56);
+                        dump.setPosition(0.49);
+                    })
+                    .waitSeconds(3)
+                    .forward(Distance_From_Board,
+                            SampleMecanumDrive.getVelocityConstraint(10, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                            SampleMecanumDrive.getAccelerationConstraint(10)
+                            //Limits to 10 in/s and 10 in/s^2
+                    )
+                    .addTemporalMarker(5.5, () -> {
+                        OperateClaw(0, 1);
+                        OperateClaw(1, 1);
+                    })
+                    .waitSeconds(1.5)
+                    .back(Distance_From_Board)
+                    .addTemporalMarker( 7.5, () -> {
+                        OperateClaw(0, 0);
+                        OperateClaw(1, 0);
+                        arm1.setPosition(0.87);
+                        arm2.setPosition(0.09);
+                        dump.setPosition(0.3);
+                    })
+                    .addTemporalMarker(9.5, () -> {
+                        slide.setPower(-0.5);
+                        slide.setTargetPosition((int) (0));
+                    })
+                    .strafeTo(new Vector2d(30,-56))
+                    .waitSeconds(4)
+                    .lineTo(new Vector2d(56,-56))
                     .build();
             drive.followTrajectorySequence(trajSeq);
-        } else {
-                /*
-                // location 3
-                // right
-                StrafeRobot(28, MainSpeed);
-                // forward 1
-                MoveRobot(65, MainSpeed);
-                MoveRobot(-25, MainSpeed);
-                StrafeRobot(27, MainSpeed);
-                MoveRobot(20, MainSpeed);
-                // right turn
-                TurnRobot(92, MainSpeed);
-                // strafe right to 3rd position
-                StrafeRobot(6, MainSpeed);
-                MoveRobot(-13, MainSpeed);
-                Place_On_Board(20, 4.5);
-                sleep(100);
-                // strafe righ 1
-                StrafeRobot(40, MainSpeed);
-                // forward 1
-                MoveRobot(60, MainSpeed);
-                sleep(1000); */
+            drive.followTrajectorySequence(On_Board);
+        } else { //Location 3, Right Side
             TrajectorySequence trajSeq = drive.trajectorySequenceBuilder(startPose)
-                    .strafeRight(13)
-                    .forward(24)
+                    .strafeRight(13,
+                            SampleMecanumDrive.getVelocityConstraint(30, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                            SampleMecanumDrive.getAccelerationConstraint(30)
+                                    //Limits to 30 in/s and 30 in/s^2
+                    )
+                    .forward(24,
+                            SampleMecanumDrive.getVelocityConstraint(30, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                            SampleMecanumDrive.getAccelerationConstraint(30)
+                                    //Limits to 30 in/s and 30 in/s^2
+                    )
                     .back(15)
                     .turn(Math.toRadians(-90))
-                    .splineTo(new Vector2d(38,-38),0)
-                    .addDisplacementMarker(()-> {
-                        //Put the "Place on board "code here.
-                        //Place_On_Board(20, 4.5)
+                    .lineTo(new Vector2d(38,-38))
+                    .build();
+            Distance_From_Board = 4; // do not set me to 0 - I will kill your code
+            Slide_Hieght = 4.5;
+            TrajectorySequence On_Board = drive.trajectorySequenceBuilder (trajSeq.end())
+                    .addTemporalMarker(0, () -> {
+                        slide.setPower(0.5);
+                        slide.setTargetPosition((int) (Slide_Hieght * 385));
                     })
-                    .splineTo(new Vector2d (60,-60),0)
+                    .addTemporalMarker(2, () -> {
+                        arm1.setPosition(0.4);
+                        arm2.setPosition(0.56);
+                        dump.setPosition(0.49);
+                    })
+                    .waitSeconds(3)
+                    .forward(Distance_From_Board,
+                            SampleMecanumDrive.getVelocityConstraint(10, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                            SampleMecanumDrive.getAccelerationConstraint(10)
+                            //Limits to 10 in/s and 10 in/s^2
+                    )
+                    .addTemporalMarker(5.5, () -> {
+                        OperateClaw(0, 1);
+                        OperateClaw(1, 1);
+                    })
+                    .waitSeconds(1.5)
+                    .back(Distance_From_Board)
+                    .addTemporalMarker( 7.5, () -> {
+                        OperateClaw(0, 0);
+                        OperateClaw(1, 0);
+                        arm1.setPosition(0.87);
+                        arm2.setPosition(0.09);
+                        dump.setPosition(0.3);
+                    })
+                    .addTemporalMarker(9.5, () -> {
+                        slide.setPower(-0.5);
+                        slide.setTargetPosition((int) (0));
+                    })
+                    .strafeTo(new Vector2d(30,-56))
+                    .waitSeconds(4)
+                    .lineTo(new Vector2d(56,-56))
                     .build();
             drive.followTrajectorySequence(trajSeq);
+            drive.followTrajectorySequence(On_Board);
         }
     }
     private void initTfod() {
@@ -314,34 +331,7 @@ public class RedBackboard extends LinearOpMode {
             telemetry.addData("- Size", JavaUtil.formatNumber(myTfodRecognition.getWidth(), 0) + " x " + JavaUtil.formatNumber(myTfodRecognition.getHeight(), 0));
         }
     }
-    private void Place_On_Board(int Distance_From_Board, double Slide_Hieght) {
-        // erect sliderail
-        slide.setPower(0.5);
-        slide.setTargetPosition((int) (Slide_Hieght * 385));
-        sleep(1500);
-        arm1.setPosition(0.4);
-        arm2.setPosition(0.56);
-        sleep(1500);
-        dump.setPosition(0.49);
-        sleep(500);
-        //MoveRobot(Distance_From_Board, MainSpeed);
-        //MoveRobot(5, MainSpeed);
-        sleep(500);
-        OperateClaw(0, 1);
-        OperateClaw(1, 1);
-        sleep(500);
-        // reverse?
-        //MoveRobot(-Distance_From_Board, MainSpeed);
-        OperateClaw(0, 0);
-        OperateClaw(1, 0);
-        dump.setPosition(0.3);
-        sleep(1500);
-        arm1.setPosition(0.87);
-        arm2.setPosition(0.09);
-        sleep(2500);
-        slide.setPower(-0.5);
-        slide.setTargetPosition(0 * 385);
-    }
+
     private void OperateClaw(int side, int status) {
         // left - bigger close smaller open
         // right - bigger open smaller close
@@ -355,88 +345,5 @@ public class RedBackboard extends LinearOpMode {
         } else if (side == 1 && status == 0) {
             rclaw.setPosition(0.27);
         }
-    }
-    private void RunToPos() {
-        fl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        fr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        bl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        br.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-    }
-
-
-    private void ResetEncoder() {
-        fl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        bl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        br.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-    }
-    private void MoveRobot(double dist, double speed) {
-        ResetEncoder();
-        // Wheel Directions
-        fl.setDirection(DcMotor.Direction.REVERSE);
-        fr.setDirection(DcMotor.Direction.FORWARD);
-        bl.setDirection(DcMotor.Direction.REVERSE);
-        br.setDirection(DcMotor.Direction.FORWARD);
-        // Set target positions to dist
-        fl.setTargetPosition((int) (dist * 12.26));
-        fr.setTargetPosition((int) (dist * 12.26));
-        bl.setTargetPosition((int) (dist * 12.26));
-        br.setTargetPosition((int) (dist * 12.26));
-        RunToPos();
-        // set power to speed
-        fl.setPower(speed);
-        fr.setPower(speed);
-        bl.setPower(speed);
-        br.setPower(speed);
-        while (fr.isBusy()) {
-        }
-        ResetEncoder();
-    }
-
-
-    private void StrafeRobot(int dist, double speed) {
-        ResetEncoder();
-        // Wheel Directions
-        fl.setDirection(DcMotor.Direction.REVERSE);
-        fr.setDirection(DcMotor.Direction.REVERSE);
-        bl.setDirection(DcMotor.Direction.FORWARD);
-        br.setDirection(DcMotor.Direction.FORWARD);
-        // Set target positions to dist
-        fl.setTargetPosition((int) (dist * 14.18));
-        fr.setTargetPosition((int) (dist * 14.18));
-        bl.setTargetPosition((int) (dist * 14.18));
-        br.setTargetPosition((int) (dist * 14.18));
-        RunToPos();
-        // set power to speed
-        fl.setPower(speed);
-        fr.setPower(speed);
-        bl.setPower(speed);
-        br.setPower(speed);
-        while (fr.isBusy()) {
-        }
-        ResetEncoder();
-    }
-
-    private void TurnRobot(int dist, double speed) {
-        ResetEncoder();
-        // Wheel Directions
-        fl.setDirection(DcMotor.Direction.REVERSE);
-        fr.setDirection(DcMotor.Direction.REVERSE);
-        bl.setDirection(DcMotor.Direction.REVERSE);
-        br.setDirection(DcMotor.Direction.REVERSE);
-        // Set target positions to dist
-        fl.setTargetPosition((int) (dist * 8.245));
-        fr.setTargetPosition((int) (dist * 8.245));
-        bl.setTargetPosition((int) (dist * 8.245));
-        br.setTargetPosition((int) (dist * 8.245));
-        RunToPos();
-        // set power to speed
-        fl.setPower(speed);
-        fr.setPower(speed);
-        bl.setPower(speed);
-        br.setPower(speed);
-        while (fr.isBusy()) {
-        }
-        ResetEncoder();
     }
 }
